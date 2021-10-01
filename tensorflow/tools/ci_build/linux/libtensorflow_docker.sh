c9b0f9f76890d240a1924741b3947ec0ef29b383
Matthew Clark <linux.matthew@gmail.com>
2021-10-01 10:16:15 -0400

    update libtensorflow_docker

--- tensorflow/tools/ci_build/linux/libtensorflow_docker.sh
+++ tensorflow/tools/ci_build/linux/libtensorflow_docker.sh
@@ -38,7 +38,7 @@ if [ "${TF_NEED_CUDA}" == "1" ]; then
   DOCKER_BINARY="nvidia-docker"
   #DOCKER_FILE="Dockerfile.rbe.cuda10.1-cudnn7-ubuntu16.04-manylinux2010"
   #DOCKER_FILE="Dockerfile.rbe.cuda10.2-cudnn7-ubuntu18.04-manylinux2010"
-  DOCKER_FILE="Dockerfile.rbe.cuda11.4-cudnn8-ubuntu18.04-manylinux2010"
+  DOCKER_FILE="Dockerfile.rbe.cuda11.4-cudnn8.2-ubuntu18.04-manylinux2010-multipython"
 fi
 if [ "${TF_NEED_ROCM}" == "1" ]; then
   DOCKER_IMAGE="tf-tensorflow-rocm"
